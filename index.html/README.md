dee2946aae7d9446c293dc7f38dc03362a975d5f
alex-sandr96 <sannyberezhnoy@gmail.com>
2025-07-31 20:40:58 +0300

    Add files via upload

---
+++ index.html/README.md
@@ -0,0 +1,6 @@
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/ocbfqzqr-the-lessful/pen/WbQRJJO](https://codepen.io/ocbfqzqr-the-lessful/pen/WbQRJJO).
+
---
+++ index.html/dist/script.js
@@ -0,0 +1,3 @@
+<a href="https://leadrock.com/URL-KJMTC-GEEJW" style="display:inline-block;padding:12px 25px;background-color:#00aaff;color:#fff;text-decoration:none;border-radius:5px;">
+    Замовити зараз
+</a>
