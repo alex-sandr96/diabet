b7bfe7a21670d66fcc57566a324bcd939a6c5608
alex-sandr96 <sannyberezhnoy@gmail.com>
2025-07-31 21:10:00 +0300

    Delete index.html directory

--- index.html/README.md
+++
@@ -1,6 +0,0 @@
-# Untitled
-
-A Pen created on CodePen.
-
-Original URL: [https://codepen.io/ocbfqzqr-the-lessful/pen/WbQRJJO](https://codepen.io/ocbfqzqr-the-lessful/pen/WbQRJJO).
-
--- index.html/dist/script.js
+++
@@ -1,3 +0,0 @@
-<a href="https://leadrock.com/URL-KJMTC-GEEJW" style="display:inline-block;padding:12px 25px;background-color:#00aaff;color:#fff;text-decoration:none;border-radius:5px;">
-    Замовити зараз
-</a>
